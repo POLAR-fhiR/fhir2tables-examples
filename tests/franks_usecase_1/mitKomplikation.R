88d0607d59ccf7dbd51921c2df5958e30d56b5b9
TPeschel <tpeschel@life.uni-leipzig.de>
2020-06-03 17:05:25 +0200

    add 3 tests

---
+++ tests/franks_usecase_1/mitKomplikation.R
@@ -0,0 +1,34 @@
+# Observation bloot pressure from loinc
+###
+# Endpunkt des fhir r4 Servers
+###
+#endpoint <-  "https://vonk.fire.ly/R4/"#
+endpoint <- "https://hapi.fhir.org/baseR4"
+
+###
+# fhir search ohne Endpunktangabe
+###
+fhir.search <- "Patient?_format=xml&_count=500"
+
+###
+# Welche Daten aus den Pages sollen wie in welchen Tabellen erzeugt werden
+# Hier nur eine Tabelle Patient mit den Einträgen PID, Geschlecht und Geburtsdatum
+tables.design <- list(
+	Patient = list(
+		entry   = ".//Patient[gender and birthDate]",
+		items = list(
+			PID  ="id/@value",
+			GENDER = "gender/@value",
+			BIRTHDATE ="birthDate/@value"
+		)
+	)
+)
+
+###
+# filtere Daten in Tabellen vor dem Export ins Ausgabeverzeichnis
+###
+filter.data <- function( lot ) {
+
+	lot
+}
+
