3f74ee7d7478a1b4f69c569dba866072ccc674fb
Jens <jens.przybilla@imise.uni-leipzig.de>
2020-06-10 13:36:22 +0200

    small changes

--- tests/jens_usecase_2/start.sh
+++ tests/jens_usecase_2/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#cd ../../
-#fhiR=$(pwd .)/allBundles.R
-#cd tests/jens_usecase_2
-#echo $fhiR
-#Rscript $fhiR -s spec-blutdruck.R -o resultBlutdruck
 cd ../../
 fhiR=$(pwd .)/allBundles.R
 cd tests/jens_usecase_2
