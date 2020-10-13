8d0ff1d2e09b39c589aec4a08f004f7411d1e493
TPeschel <tpeschel@life.uni-leipzig.de>
2020-10-14 01:07:18 +0200

    Add Example for 2 Mio Observations

---
+++ examples/sequentially_download/run-sequentially_download.sh
@@ -0,0 +1,1 @@
+Rscript sequentially_download.R
---
+++ examples/sequentially_download/sequentially_download.R
@@ -0,0 +1,95 @@
+rm( list = ls( ) )
+
+#setwd("examples/sequentially_download/")
+
+###
+# https://polar-fhir.github.io/fhircrackr/
+###
+
+# install v0.1.2
+devtools::install_github( "POLAR-fhir/fhircrackr" )
+
+library( "fhircrackr" )
+
+endpoint <- "https://blaze.life.uni-leipzig.de/fhir"
+
+fhir_search_request <- paste0(
+	"Observation?",
+	"_count=500" )
+
+fsr <- fhircrackr::paste_paths( path1 = endpoint, path2 = fhir_search_request )
+#fsr <- "https://blaze.life.uni-leipzig.de/fhir/Observation?_count=500&__t=15065&__page-id=f46f38a7-d915-4e25-9bf3-52a0d8c0f9d1"
+
+max_bundles <- 200
+
+design <- list(
+	Observations = list(
+		"//Observation"
+	)
+)
+
+separator <- " | "
+
+brackets <- c("<", ">")
+
+post_processing <- function( lot ) {
+
+	# return list of tables
+	lot
+}
+
+output_directory <- "results_fix_6h"
+
+if( ! dir.exists( output_directory ) ) {
+	
+	dir.create( output_directory, recursive = T )
+}
+
+back <- getwd( )
+
+setwd( output_directory )
+
+cnt <- 0
+
+while(! is.null(fsr)) {
+
+	bundles <- fhircrackr::fhir_search( request = fsr, max_bundles = max_bundles, verbose = 2 )
+
+	list_of_tables <- fhircrackr::fhir_crack( bundles = bundles, design = design, sep = separator, brackets = brackets )
+
+	list_of_tables <- post_processing( list_of_tables )
+
+	for( n in names( list_of_tables ) ) {
+
+		write.table( list_of_tables[[ n ]], file = paste0( n, "_", cnt, ".csv" ), na = "", sep = ";", dec = ".", row.names = F, quote = F )
+	}
+
+	save( list_of_tables, file = paste0("tables_", cnt, ".RData" ))
+
+	cnt <- cnt + 1
+
+	fsr <- fhircrackr::fhir_next_bundle_url()
+}
+
+all <- list()
+
+for(fn in dir(pattern = "RData")) {
+	load(fn)
+	for( n in names(list_of_tables)) {
+		all[[n]][[fn]] <- list_of_tables[[n]]
+	}
+}
+
+all <- lapply(
+	all,
+	function(lot) {
+		data.table::rbindlist(lot, fill = TRUE)
+	}
+)
+
+save(all, file = "all.RData")
+
+setwd( back )
+
+cat( "   fin\n" )
+
